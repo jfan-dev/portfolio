b603b57f2115cffa2c11b2089624fccf77f504cc
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-30 18:25:41 -0300

    Create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NavElementComponent } from './navigation/nav-element/nav-element.component';
 import { NavLogoComponent } from './navigation/nav-logo/nav-logo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     NavElementComponent,
-    NavLogoComponent
+    NavLogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
