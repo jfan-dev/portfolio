788c5e91b980ae897eec72fac276d789f21af029
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-30 15:15:30 -0300

    Create navigation bar element scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NavElementComponent } from './navigation/nav-element/nav-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    NavElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation/nav-element/nav-element.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-element',
+  templateUrl: './nav-element.component.html',
+  styleUrls: ['./nav-element.component.css']
+})
+export class NavElementComponent {
+
+}
