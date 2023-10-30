cec0f00084b5fc54c6edf402fcf4b56d24c38097
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-30 15:16:13 -0300

    Create navigation bar logo scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NavElementComponent } from './navigation/nav-element/nav-element.component';
+import { NavLogoComponent } from './navigation/nav-logo/nav-logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    NavElementComponent
+    NavElementComponent,
+    NavLogoComponent
   ],
   imports: [
     BrowserModule,
