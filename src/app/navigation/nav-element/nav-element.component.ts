3fc328c1288840c9339346fbb8738aa5e33fd062
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-10-31 17:09:35 -0300

    navbar

--- src/app/navigation/nav-element/nav-element.component.ts
+++ src/app/navigation/nav-element/nav-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-element',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-element.component.css']
 })
 export class NavElementComponent {
+
+  @Input()
+  btnTxt:string = "";
+  btnLink:string = "";
 
 }
