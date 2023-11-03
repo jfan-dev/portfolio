6156733edf96b772dec31ce39024ba97d775a3ab
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-11-03 09:45:22 -0300

    create portfolio card scaffold

---
+++ src/app/home/portfolio/portfolio-card/portfolio-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-card',
+  templateUrl: './portfolio-card.component.html',
+  styleUrls: ['./portfolio-card.component.css']
+})
+export class PortfolioCardComponent {
+
+}
