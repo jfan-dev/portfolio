4faa0e350217639639f7b45f4165758e21222058
jfan-dev <122750665+jfan-dev@users.noreply.github.com>
2023-11-03 19:51:27 -0300

    Create and config portfolio card

--- src/app/home/portfolio/portfolio-card/portfolio-card.component.ts
+++ src/app/home/portfolio/portfolio-card/portfolio-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio-card.component.css']
 })
 export class PortfolioCardComponent {
+  
+  @Input() isReversed: boolean = true;
 
 }
